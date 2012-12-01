ab013a52842b9ef9b75e29f641143e8f0310e97b
shenhx <15999850214@139.com>
2012-12-01 19:42:45 +0800

    caozuoxitong shiyan san quanbu

---
+++ 10207160518__沈鸿校/沈鸿校-10207160518操作系统实验三/program4.c
@@ -0,0 +1,64 @@
+#include <unistd.h>
+    #include <stdio.h>
+    #include <signal.h>
+   #include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h> 
+    
+    void waiting(), stop();
+    int wait_mark;
+    main()
+    {
+        int p1, p2;
+        while((p1=fork())==-1);         /*创建进程p1*/
+        if (p1>0)
+        {
+            while((p2=fork())==-1); 
+            if (p2>0)
+            {
+                printf("parent\n");
+                wait_mark=1;
+                signal(SIGINT, stop);   /* 接收'Del'信号并转stop */
+                waiting();
+                kill(p1, 16);           /* 向p1发中断信号16 */
+                kill(p2, 17);           /* 向p2发中断信号17 */
+                wait(0);                /* 同步 */
+                wait(0);
+                printf("parent process is killed!\n");
+                exit(0);
+            }
+            else
+            {
+                printf("p2\n");
+ 		wait_mark=1;
+		signal(SIGINT,SIG_IGN);  /*忽略^c信号*/
+                signal(17, stop);
+                waiting();
+                lockf((int)stdout, 1, 0);
+                printf(" child process 2 is killed by parent! \n");
+                lockf((int)stdout, 0, 0);
+                exit(0);
+            }
+        }
+        else
+        {
+            printf("p1\n");
+            wait_mark=1;
+	    signal(SIGINT,SIG_IGN);  /*忽略^c信号*/
+            signal(16, stop);
+            waiting();
+            lockf((int)stdout, 1, 0);
+            printf(" child process 1 is killed by parent! \n");
+            lockf((int)stdout, 0, 0);
+            exit(0);
+        }
+    }
+    
+    void waiting()
+    {
+        while(wait_mark!=0);
+    }
+void stop()
+    {
+        wait_mark=0;
+    }
